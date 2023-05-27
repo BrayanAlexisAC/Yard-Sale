28293501178715c107705ea6e8ed52bc8e80225d
Brayan Alexis <brayan.aguayo@bigo.mx>
2023-05-27 13:10:11 -0600

    merge shopping cart in index (desktop and mobil)e

--- js/main.js
+++ js/main.js
@@ -1,18 +1,55 @@
 document.addEventListener('DOMContentLoaded',function(){
   //Constants
   const navbarAccount = document.querySelector('.nav-bar-account');
+  const navBarLogoMobile = document.querySelector('.nav-bar-logo-mobile');
   const btnNavBarMenu = document.querySelector('.nav-bar-menu');
+  const btnShoppingCart = document.querySelector('.nav-bar-cart');
+  const btnShoppingCartClose = document.querySelector('.shpng-cart-close');
   const btnClose = document.querySelector('.btn-close');
 
   //Events
   navbarAccount.addEventListener('click',function(){
     toggleMenuDesktop(document.querySelector('.menu-desktop'));
   });
+
   btnNavBarMenu.addEventListener('click',function(){
     toggleMenuDesktop(document.querySelector('.menu-mobile'));
   });
+
   btnClose.addEventListener('click',function(){
     toggleMenuDesktop(document.querySelector('.menu-mobile'));
+  });
+
+  btnShoppingCart.addEventListener('click',function(){
+    let shoppingCart = document.querySelector('.shopping-cart'); 
+    let isShoppingCartOpen = !shoppingCart.classList.contains('inactive');
+    console.log(isShoppingCartOpen);
+    if(isShoppingCartOpen){
+      if(window.innerWidth <= 991){
+        navBarLogoMobile.style.display = "flex";
+      }
+      toggleMenuDesktop(document.querySelector('.shpng-cart-title'));
+    }else{
+      if(window.innerWidth <= 991){
+        navBarLogoMobile.style.display = "none";
+      }
+      toggleMenuDesktop(document.querySelector('.shpng-cart-title'));
+    }
+    toggleMenuDesktop(shoppingCart);
+  });
+
+  btnShoppingCartClose.addEventListener('click',function(){
+    toggleMenuDesktop(document.querySelector('.shopping-cart'));
+  });
+
+  window.addEventListener('resize', () => {
+    let shoppingCart = document.querySelector('.shopping-cart'); 
+    let isShoppingCartOpen = !shoppingCart.classList.contains('inactive');
+    if(window.innerWidth >= 991){
+      navBarLogoMobile.style.removeProperty('display');
+    }else if(window.innerWidth <= 990 && isShoppingCartOpen){
+      navBarLogoMobile.style.display = "none";
+    }
   });
 
   //Functions
@@ -20,4 +57,5 @@ document.addEventListener('DOMContentLoaded',function(){
     // console.info(component);
     component.classList.toggle('inactive');
   }
+
 });
