eb8e6e88b903fb747db4f26d3cd4643ae8c96d4e
Brayan Alexis <brayan.aguayo@bigo.mx>
2023-05-27 10:05:12 -0600

    add menu mobile and its functionality also change constants and structure in main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-document.addEventListener('DOMContentLoaded',function(event){
-  console.info("Charged");
+document.addEventListener('DOMContentLoaded',function(){
   //Constants
   const navbarAccount = document.querySelector('.nav-bar-account');
-  const menuDesktop = document.querySelector('.menu-desktop');
+  const btnNavBarMenu = document.querySelector('.nav-bar-menu');
+  const btnClose = document.querySelector('.btn-close');
 
   //Events
-  navbarAccount.addEventListener('click',toggleMenuDesktop);
+  navbarAccount.addEventListener('click',function(){
+    toggleMenuDesktop(document.querySelector('.menu-desktop'));
+  });
+  btnNavBarMenu.addEventListener('click',function(){
+    toggleMenuDesktop(document.querySelector('.menu-mobile'));
+  });
+  btnClose.addEventListener('click',function(){
+    toggleMenuDesktop(document.querySelector('.menu-mobile'));
+  });
 
   //Functions
-  function toggleMenuDesktop(event){
-    menuDesktop.classList.toggle('inactive');
+  function toggleMenuDesktop(component){
+    // console.info(component);
+    component.classList.toggle('inactive');
   }
 });
