ca557b1f531c8c6a910b8b154654ebd9305c99a5
Brayan Alexis <brayan.aguayo@bigo.mx>
2023-05-27 09:21:58 -0600

    Move import script main.js to top index and add eventlistener document.addEventListener('DOMContentLoaded',function(event){

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-//Constants
-const navbarAccount = document.querySelector('.nav-bar-account');
-const menuDesktop = document.querySelector('.menu-desktop');
+document.addEventListener('DOMContentLoaded',function(event){
+  console.info("Charged");
+  //Constants
+  const navbarAccount = document.querySelector('.nav-bar-account');
+  const menuDesktop = document.querySelector('.menu-desktop');
 
-//Events
-navbarAccount.addEventListener('click',toggleMenuDesktop);
+  //Events
+  navbarAccount.addEventListener('click',toggleMenuDesktop);
 
-//Functions
-function toggleMenuDesktop(event){
-  menuDesktop.classList.toggle('inactive');
-}
+  //Functions
+  function toggleMenuDesktop(event){
+    menuDesktop.classList.toggle('inactive');
+  }
+});
